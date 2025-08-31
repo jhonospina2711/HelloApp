e7b6afa1cd32a8c62f04baeba22520986641d89e
Jhon Ospina <jhonospina2711@gmail.com>
2025-08-30 23:33:40 -0500

    Tipos de datos primitivos y de referencia

--- obj/Debug/net9.0/HelloApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc99b2561314d58361ff48154620d3687d62da49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cbd67fe79c3f954755954cfd696f92348abedda")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
