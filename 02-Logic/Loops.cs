43d4f62f6cb93f4cdfaaf708a94b7b8786e57adc
Jhon Ospina <jhonospina2711@gmail.com>
2025-09-13 01:06:47 -0500

    for, foreach, list

--- 02-Logic/Loops.cs
+++ 02-Logic/Loops.cs
@@ -6,7 +6,7 @@ partial class Program
         int counter = 1;
         while (counter <= 5)
         {
-            WriteLine($"Iteranción: {counter}");
+            //WriteLine($"Iteranción: {counter}");
             counter++;
         }
 
@@ -14,10 +14,33 @@ partial class Program
         int number = 0;
         do
         {
-            WriteLine($"Número: {number}");
+            //WriteLine($"Número: {number}");
             number++;
 
         } while (number < 3);
+
+        //For: Se usa cuando se tiene la certeza de cuantas iteraciones se van a realizar
+        for (int i = 0; i <= 5; i++)
+        {
+            WriteLine($"Iteración: {i}");
+        }
+        for (int i = 10; i >= 0; i -= 2)
+        {
+            WriteLine(i);
+        }
+        //For Each: Se usa para recorrer colecciones
+        //Array
+        string[] fruits = { "Manzana", "Pera", "Piña" };
+        foreach (var fruit in fruits)
+        {
+            WriteLine(fruit);
+        }
+
+        //List
+        List<string> names = ["Pedro", "Luis", "Nancy"];
+        foreach (var name in names) {
+            WriteLine(name);
+        }
 
     }
 
