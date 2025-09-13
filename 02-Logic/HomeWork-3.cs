3f44804dd83de7b2df1888f2909177c6a0eb9a02
Jhon Ospina <jhonospina2711@gmail.com>
2025-09-13 01:31:27 -0500

    HomeWork3

---
+++ 02-Logic/HomeWork-3.cs
@@ -0,0 +1,34 @@
+partial class Program
+{
+    // 🏆 Ejercicio:
+    // Implementar un programa que recorra los números del 1 al 100.
+    // Consideraciones:
+    // Aplicar la lógica de FizzBuzz:
+    // - Si un número es divisible por 3, imprimir "Fizz".
+    // - Si un número es divisible por 5, imprimir "Buzz".
+    // - Si un número es divisible por 3 y 5, imprimir "FizzBuzz".
+    // - En cualquier otro caso, imprimir el número.
+    // 💡 Pista:
+    // - El orden de las condiciones es clave.
+    // Piensa en cómo asegurarte de que "FizzBuzz" se imprima antes que "Fizz" o "Buzz".
+
+    static void FizzBuzz()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                WriteLine("FizzBuzz");
+            } else if (i % 3 == 0) {
+                WriteLine("Fizz");
+            } else if (i % 5 == 0) {
+                WriteLine("Buzz");
+            } else {
+                WriteLine(i);
+            }
+        }
+
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ partial class Program
         // DataStructures();
         // Generics();
         // Conditionals();
-        Loops();
+        // Loops();
+        FizzBuzz();
     }
 }
