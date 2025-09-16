63e6d78422b5d753eacd4c934b3f0a6e3a139161
Jhon Ospina <jhonospina2711@gmail.com>
2025-09-16 00:17:54 -0500

    Tarea extraer los factoriales

--- 02-Logic/HomeWork-4.cs
+++ 02-Logic/HomeWork-4.cs
@@ -7,7 +7,37 @@ partial class Program
         {
             WriteLine($"{number} x {i} = {number * i}");
         }
-        
 
+
+    }
+
+    // 🏆 Ejercicio:
+    // Crear un método llamado `PrintFactorialTable` que reciba un número 
+    // y muestre el factorial de todos los números desde 1 hasta el número ingresado.
+    // Ejemplo: PrintFactorialTable(5);
+    // 1! = 1
+    // 2! = 2
+    // 3! = 6
+    // 4! = 24
+    // 5! = 120
+
+    static void PrintFactorialTable(int number)
+    {
+        WriteLine($"Factorial del número {number} desde el 1 hasta el {number} \n");
+
+        for (int i = 1; i <= number; i++)
+        {
+            WriteLine($"{i}! = {Factorial(i)}");
+        }
+    }
+
+    static int Factorial(int number)
+    {
+        int factorial = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+        return factorial;
     }
 }
