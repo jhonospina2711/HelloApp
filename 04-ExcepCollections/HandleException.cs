47e4395ac9d55e1ef75d242ef00ddf3c5427f146
Jhon Ospina <jhonospina2711@gmail.com>
2025-10-12 23:41:27 -0500

    Manejo de excepciones

--- 04-ExcepCollections/HandleException.cs
+++ 04-ExcepCollections/HandleException.cs
@@ -12,8 +12,19 @@ partial class Program
             amount = ReadLine();
             if (string.IsNullOrEmpty(amount)) return;
 
-            double amountValue = double.Parse(amount);
-            WriteLine($"El monto ingresado es: {amountValue:C}");
+            if (double.TryParse(amount, out double amountValue))
+            {
+                WriteLine($"El monto ingresado es: {amountValue:C}");
+            }
+            else
+            {
+                WriteLine("No se pudo convertir el texto a número.");
+            }
+
+            //double amountValue = double.Parse(amount);
+            ValidateAge(16);
+
+
         }
         catch (DivideByZeroException ex)
         {
@@ -22,12 +33,28 @@ partial class Program
         }
         catch (FormatException) when (amount?.Contains("$") == true)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             WriteLine("No es necesario ingresar el símbolo de $");
         }
         catch (Exception ex)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             WriteLine($"Error: {ex.Message}");
         }
+        finally
+        {
+            Console.ResetColor();
+            WriteLine("Esto siempre se ejecuta.");
+        }
 
+    }
+
+    static void ValidateAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArgumentException("La edad debe ser mayor o igual a 18.");
+        }
+        
     }
 }
